a2794d629eb95c5ae806cec1d0b3a2f83bb08ed2
Dipesh <dipeshluitel40@gmail.com>
2026-03-24 11:09:23 +0545

    written consumer for streaming raw layer

---
+++ streaming/consumer.py
@@ -0,0 +1,34 @@
+import json
+import os
+from kafka import KafkaConsumer
+from datetime import datetime
+
+#kafka consumer setup
+
+consumer = KafkaConsumer(
+    'crypto_prices',
+    bootstrap_servers = 'localhost:9092',
+    auto_offset_reset = 'earliest',
+    value_deserializer = lambda x: json.loads(x.decode('utf-8'))
+)
+
+def save_raw(message):
+    os.makedirs('data/straming_raw',exist_ok=True)
+
+    filename = f"data/streaming_raw/{datetime.now().date()}.json"
+
+    with open(filename,"a") as f:
+        f.write(json.dump(message)+ "\n")
+    
+
+def main():
+    print("Consumer Requesting--")
+
+    for msg in consumer:
+        data = msg.value
+        print(f"Received: {data}")
+
+        save_raw(data)
+
+if __name__ == "__main__":
+    main()
