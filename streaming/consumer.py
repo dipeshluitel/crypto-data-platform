99b7a7d04b290e4b3f7286acfe9249abbbc1a563
Dipesh <dipeshluitel40@gmail.com>
2026-03-26 20:59:00 +0545

    Update(consumer): Now data is stored within postgres Database

--- streaming/consumer.py
+++ streaming/consumer.py
@@ -2,6 +2,7 @@ import json
 import os
 from kafka import KafkaConsumer
 from datetime import datetime
+import psycopg2
 
 #kafka consumer setup
 
@@ -12,23 +13,47 @@ consumer = KafkaConsumer(
     value_deserializer = lambda x: json.loads(x.decode('utf-8'))
 )
 
-def save_raw(message):
-    os.makedirs('data/streaming_raw',exist_ok=True)
+# postgres Connection
 
-    filename = f"data/streaming_raw/{message['coin']}_{datetime.now().date()}.json"
+conn = psycopg2.connect(
+    host = "localhost",
+    database = "crypto_db",
+    user = "admin",
+    password = "admin"
+)
+cur = conn.cursor()
 
-    with open(filename,"a") as f:
-        f.write(json.dumps(message) + "\n")
+def insert_raw(data):
+    query = """ INSERT INTO crypto_prices(coin, price, timestamp) VALUES(%s, %s, %s)"""
+    cur.execute(query, (data['coin'],data['price'],datetime.fromtimestamp(data['timestamp'])))
+    conn.commit()
+
+# def save_raw(message):
+#     os.makedirs('data/streaming_raw',exist_ok=True)
+
+#     filename = f"data/streaming_raw/{message['coin']}_{datetime.now().date()}.json"
+
+#     with open(filename,"a") as f:
+#         f.write(json.dumps(message) + "\n")
     
 
+# def main():
+#     print("Consumer Requesting--")
+
+#     for msg in consumer:
+#         data = msg.value
+#         print(f"Received: {data}")
+
+#         save_raw(data)
+
 def main():
-    print("Consumer Requesting--")
+    print("Consumer Requesting and Stroing raw Data--")
 
     for msg in consumer:
         data = msg.value
-        print(f"Received: {data}")
+        print(f"Inserted : {data}")
 
-        save_raw(data)
+        insert_raw(data)
 
 if __name__ == "__main__":
     main()
